1562c47c7c39045ed4aa884c237e4698ad20c761
Tanvi <agarwll2@illinois.edu>
2020-10-11 00:34:35 -0700

    Updates

---
+++ precache-manifest.c6defac642c0f696f734fcfa7e10960e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6d6190b4d2d8ef721f5220cbc341dd",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "a2d3771f56f5259b15a0",
+    "url": "/birthday/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8107a4518ab9820a30e",
+    "url": "/birthday/static/js/2.4e299865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday/static/js/2.4e299865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d3771f56f5259b15a0",
+    "url": "/birthday/static/js/main.ee39f6fc.chunk.js"
+  },
+  {
+    "revision": "a80f4ddd0bebb5917654",
+    "url": "/birthday/static/js/runtime-main.27fdd891.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/birthday/static/media/logo.5d5d9eef.svg"
+  }
+]);
